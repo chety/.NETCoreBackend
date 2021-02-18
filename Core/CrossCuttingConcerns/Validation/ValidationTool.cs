9de0d9f35ea6c962a750f1fd5952e4171d44377f
Chety <cetincelik20@gmail.com>
2021-02-18 16:38:55 +0300

    Add files via upload
    Add CrossCuttingConcerns folder

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    //Another implementation
+    //public class ValidationTool<TEntity, TValidator> where TValidator : AbstractValidator<TEntity>, new()
+    //{
+    //    public static void Validate(TEntity entity)
+    //    {
+    //        var context = new ValidationContext<TEntity>(entity);
+    //        var validator = new TValidator();
+    //        var result = validator.Validate(context);
+    //        if (!result.IsValid) throw new ValidationException(result.Errors);
+    //    }
+    //}
+
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid) throw new ValidationException(result.Errors);
+        }
+    }
+}
