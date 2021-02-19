ba907aea4d069b5808bc5507b76e24c35bc499e1
Chety <cetincelik20@gmail.com>
2021-02-19 14:36:51 +0300

    Update AutofacBusinessModule.cs

--- Business/DependencyResolvers/AutoFac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/AutoFac/AutofacBusinessModule.cs
@@ -1,6 +1,8 @@
-﻿using Autofac;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -12,6 +14,14 @@ namespace Business.DependencyResolvers.AutoFac
         {
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                .AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
