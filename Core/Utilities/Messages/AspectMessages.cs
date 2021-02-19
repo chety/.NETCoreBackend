047fe962efc37edad0e353d2545648c1109f5f86
Chety <cetincelik20@gmail.com>
2021-02-19 14:40:22 +0300

    Add files via upload

---
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Messages
+{
+    internal static class AspectMessages
+    {
+        public static string WrongValidationType { get; } = "Wrong Validation Type";
+    }
+}
