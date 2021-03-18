1916e7eaebd63672a12f2785be189515f48e8992
Chety <cetincelik20@gmail.com>
2021-03-18 16:10:24 +0300

    Add files via upload

---
+++ Core/Aspects/Autofac/Performance/PerformanceAspect.cs
@@ -0,0 +1,33 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace Core.Aspects.Autofac.Performance
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private readonly int _interval;
+        private readonly Stopwatch _stopwatch;
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwatch = ServiceTool.ServiceProvider.GetService<Stopwatch>();
+        }
+        protected override void OnBefore(IInvocation _)
+        {
+            _stopwatch.Start();
+        }
+        protected override void OnAfter(IInvocation invocation)
+        {
+            double totalSeconds = _stopwatch.Elapsed.TotalSeconds;
+            //if our service lasts more than we expected then take action
+            if (totalSeconds > _interval)
+            {
+                Debug.WriteLine($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name} takes {totalSeconds} seconds");
+            }
+            _stopwatch.Reset();
+        }
+    }
+}
---
+++ Core/Aspects/Autofac/Transaction/TransactionAspect.cs
@@ -0,0 +1,27 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (var scope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    scope.Complete();
+                }
+                catch (Exception)
+                {
+                    scope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
