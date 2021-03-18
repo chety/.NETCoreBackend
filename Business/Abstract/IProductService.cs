eddf31b60f8697b5d3513b781f64d2d6cc1b8bf4
Chety <cetincelik20@gmail.com>
2021-03-18 16:14:35 +0300

    Update IProductService.cs

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -15,6 +15,6 @@ namespace Business.Abstract
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
         IResult AddProduct(Product product);
-
+        IResult AddProductWithTransaction(Product product);
     }
 }
